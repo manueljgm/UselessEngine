617a0fa321d7c5390a4612f7cc6ed3539afa754e
Manuel Martins <manueljgm@gmail.com>
2020-07-17 09:46:34 -0400

    Initial Commit

---
+++ UselessEngine/UselessEngine.h
@@ -0,0 +1,19 @@
+//
+//  UselessEngine.h
+//  UselessEngine
+//
+//  Created by Manny Martins on 7/17/20.
+//  Copyright © 2020 Useless Robot. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UselessEngine.
+FOUNDATION_EXPORT double UselessEngineVersionNumber;
+
+//! Project version string for UselessEngine.
+FOUNDATION_EXPORT const unsigned char UselessEngineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UselessEngine/PublicHeader.h>
+
+

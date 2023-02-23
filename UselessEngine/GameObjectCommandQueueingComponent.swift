e27725e8fc4f37b4dc246a4415a184c3bd36f8cf
Manuel Martins <manueljgm@gmail.com>
2023-02-22 21:15:30 -0500

    Change GameObjectCommandQueueingComponent queue processing to process by priority and timestamp order

--- UselessEngine/GameObjectCommandQueueingComponent.swift
+++ UselessEngine/GameObjectCommandQueueingComponent.swift
@@ -9,21 +9,29 @@
 open class GameObjectCommandQueueingComponent: GameObjectInputComponent {
 
     /// Queued input commands.
-    public private(set) var commandQueue: [(command: GameObjectCommand, payload: AnyObject?)] = []
+    public private(set) var commandQueue: [(command: GameObjectCommand, ts: TimeInterval, payload: AnyObject?)] = []
     
     public init() {
 
     }
 
-    public func queue(command: GameObjectCommand, payload: AnyObject?) {
-        if (command.priority == .urgent) {
+    public func queue(command newCommand: GameObjectCommand, payload: AnyObject?) {
+        if (newCommand.priority == .urgent) {
             // if urgent, override all commands
-            commandQueue = [(command, payload)]
+            commandQueue = [(newCommand, Date.timeIntervalSinceReferenceDate, payload)]
         } else {
-            // else keep commands with higher priority than this command
-            commandQueue = commandQueue.filter({ $0.command.priority > command.priority })
-            // including the new one
-            commandQueue.insert((command, payload), at: 0)
+            // clear commands on the same priority as this command
+            // TODO: revisit this
+            commandQueue.removeAll(where: { $0.command.priority == newCommand.priority })
+            // and add the new command
+            commandQueue.append((newCommand, Date.timeIntervalSinceReferenceDate, payload))
+            // finally sort by priority and then timestamp
+            commandQueue.sort {
+                guard $0.command.priority != $1.command.priority else {
+                    return $0.ts > $1.ts
+                }
+                return $0.command.priority < $1.command.priority
+            }
         }
     }
     

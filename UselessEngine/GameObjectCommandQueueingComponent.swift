544f449592d0dc362ef6785b4289aa054171ee5e
Manuel Martins <manueljgm@gmail.com>
2023-03-10 10:05:16 -0500

    Keep queued commands of the same priority while clearing duplicates of the newly issued command for more predicable execution

--- UselessEngine/GameObjectCommandQueueingComponent.swift
+++ UselessEngine/GameObjectCommandQueueingComponent.swift
@@ -20,9 +20,8 @@ open class GameObjectCommandQueueingComponent: GameObjectInputComponent {
             // if urgent, override all commands
             commandQueue = [(newCommand, Date.timeIntervalSinceReferenceDate, payload)]
         } else {
-            // clear commands on the same priority as this command
-            // TODO: revisit this
-            commandQueue.removeAll(where: { $0.command.priority == newCommand.priority })
+            // clear any previous calls to the same command
+            commandQueue.removeAll(where: { $0.command == newCommand })
             // and add the new command
             commandQueue.append((newCommand, Date.timeIntervalSinceReferenceDate, payload))
             // finally sort by priority and then timestamp

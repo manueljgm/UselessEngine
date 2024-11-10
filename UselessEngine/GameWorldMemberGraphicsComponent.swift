a9936bff1dfe70f1451488c436e3c990da46fc5c
Manuel Martins <manueljgm@gmail.com>
2024-11-10 16:24:05 -0500

    Add position transform requirement and default update implementation to protocol GameWorldMemberGraphicsComponent

--- UselessEngine/GameWorldMemberGraphicsComponent.swift
+++ UselessEngine/GameWorldMemberGraphicsComponent.swift
@@ -11,7 +11,18 @@ import SpriteKit
 public protocol GameWorldMemberGraphicsComponent: GameWorldMemberObserver {
 
     var sprite: SKSpriteNode { get }
+    var transform: (_ position: Position) -> (point: CGPoint, zIndex: CGFloat) { get set }
 
     func update(with owner: GameWorldMember, dt: Float)
 
+}
+
+public extension GameWorldMemberGraphicsComponent {
+    
+    func update(with owner: GameWorldMember, dt: Float) {
+        let viewspacePosition = transform(owner.position)
+        sprite.position = viewspacePosition.point
+        sprite.zPosition = viewspacePosition.zIndex
+    }
+    
 }
